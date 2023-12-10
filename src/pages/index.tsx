4e0e4e0c8b7b82935bfd8e11f13bea1f40ab65fa
Juliana Otero <julianaotero@MacBook-Air-de-Juliana.local>
2023-12-10 11:46:00 -0300

    Add router for posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import Head from "next/head";
-import Link from "next/link";
 
 import { api } from "~/utils/api";
 
 export default function Home() {
   const user = useUser();
+  const { data } = api.post.getAll.useQuery();
   return (
     <>
       <Head>
@@ -14,8 +14,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {!user.isSignedIn && <SignInButton></SignInButton>}
-        {!!user.isSignedIn && <SignOutButton></SignOutButton>}
+        <div>
+          {!user.isSignedIn && <SignInButton></SignInButton>}
+          {!!user.isSignedIn && <SignOutButton></SignOutButton>}
+        </div>
+        <div>
+          {data?.map((post) => <div key={post.id}>{post.content}</div>)}
+        </div>
       </main>
     </>
   );
